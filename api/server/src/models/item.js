9ccbc3df1cf43099c90b53768781dd51f3981cfd
MaruniakS <st.maruniak@gmail.com>
2019-11-27 20:02:44 +0200

    sequalize config; item service

---
+++ api/server/src/models/item.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Item = sequelize.define('Item', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return Item;
+};
